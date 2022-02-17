b23432a97192e9de9b1b53e1a79fe8c7ed5f12e7
Shantanu-14 <b519043@iiit-bh.ac.in>
2022-02-17 16:51:27 +0530

    first Commit

---
+++ config/keys.js
@@ -0,0 +1,4 @@
+module.exports = {
+  MONGO_URI:
+    "mongodb://Shantanu:textshare@cluster0-shard-00-00.enczt.mongodb.net:27017,cluster0-shard-00-01.enczt.mongodb.net:27017,cluster0-shard-00-02.enczt.mongodb.net:27017/myFirstDatabase?ssl=true&replicaSet=atlas-eqa96e-shard-0&authSource=admin&retryWrites=true&w=majority",
+};
